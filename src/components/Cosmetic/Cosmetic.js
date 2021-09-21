366cefffa91723f8ba1226e9be1c92e8c21cf2e0
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-22 01:16:24 +0600

    add and remove data to the localstorage

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, deleteFromDb } from '../../utilities/fakedb';
 
 const Cosmetic = (props) => {
     // console.log(props.cosmetic);
@@ -10,12 +10,16 @@ const Cosmetic = (props) => {
         console.log(id);
         addToDb(id);
     }
+    const handleRemove = id => {
+        deleteFromDb(id);
+    }
     return (
         <div>
             <h3>Name: {name}</h3>
             <h4>Id:{_id} Balance: {balance}</h4>
             <p> <small>Gender: {gender}</small></p>
             <button onClick={() => handlePurchase(_id)}>Purchase</button>
+            <button onClick={() => handleRemove(_id)}>Remove</button>
         </div>
     );
 };
--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,16 +1,46 @@
 //Use localstorage as your db now
 
 const addToDb = id => {
-    const exists = localStorage.getItem(id);
-    console.log(exists);
+    const exists = getDb();
+    // console.log(exists);
+    let shopping_cart = {};
     if (!exists) {
-        localStorage.setItem(id, 1);
+        shopping_cart[id] = 1;
     }
     else {
-        const newCount = parseInt(exists) + 1;
-        localStorage.setItem(id, newCount);
+        shopping_cart = JSON.parse(exists);
+        if (shopping_cart[id]) {
+            const newCount = shopping_cart[id] + 1;
+            shopping_cart[id] = newCount;
+        }
+        else {
+            shopping_cart[id] = 1;
+        }
     }
+    updateDb(shopping_cart);
 
 }
 
-export { addToDb }
+const getDb = () => localStorage.getItem('shopping-cart');
+const updateDb = cart => {
+    localStorage.setItem('shopping-cart', JSON.stringify(cart));
+}
+
+const removeFromDb = id => {
+    const exists = getDb();
+    if (!exists) {
+
+    }
+    else {
+        const shopping_cart = JSON.parse(exists);
+        delete shopping_cart[id];
+        updateDb(shopping_cart);
+    }
+}
+
+export { addToDb, removeFromDb as deleteFromDb }
+
+/*
+        const newCount = parseInt(exists) + 1;
+        localStorage.setItem(id, newCount);
+*/
